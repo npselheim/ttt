4510796a0d84db0d64c491e8101de05500b6d975
Norman Selheim <npselheim@gmail.com>
2013-02-13 09:40:03 -0500

    eliminate _mark variable
    also, remove _ prefixes from all variables

--- app/javascripts/src/ttt/cell.js
+++ app/javascripts/src/ttt/cell.js
@@ -9,25 +9,19 @@ MyApp.cell = function (index) {
       * @type {Element}
       * @private
       */
-    var _cellRef,
+    var cellRef,
     /** 
-      * the player that marked this cell
+      * the mark in this cell
       * @type {Player}
       * @private
       */
-        _player = '',
-    /**
-      * indicates whether the cell is 'taken'
-      * @type {Boolean}
-      * @private
-      */
-        _isMarked = false;
+        mark = '';
 
     // verify that index is in range 0-8
     if ( index < 0 || index > 8 ) {
         throw new Error( "index must be in range 0-8" );
     }
-    _cellRef = $('cell' + index);
+    cellRef = $('cell' + index);
 
     /** @scope MyApp.cell */
     return {  
@@ -36,39 +30,40 @@ MyApp.cell = function (index) {
           * i.e. records the player's move.
           * @ param {Player} player the player that moved on this cell
           */
-        setPlayer: function (value) {
-            _player = value;
-            _isMarked = true;
+        setMark: function (value) {
+            if ( value !== 'X' && value !== 'O' ) {
+                throw new Error( "mark must be 'X' or 'O'" );
+            };
+            mark = value;
         },
         /**
           * Retrieves a reference to the player who marked this cell
           * @return {Player} the player who marked this cell
           */
-        getPlayer: function() {
-            return _player;
+        getMark: function() {
+            return mark;
         },
         /**
           * Indicates the state of the cell, marked or not
           * @return {Boolean} true if the cell has been marked
           */
         isMarked: function () {
-            return _isMarked;
+            return mark ? true : false;
         },
         /**
           * Show that this cell is included in the winning 3-in-a-row.
           * @return nothing
           */
         showWin: function () {
-            _cellRef.attr("class", "tokenWin");
+            cellRef.attr("class", "tokenWin");
         },
         /**
           * Reset this this cell back to the initial unused state.
           * @return nothing
           */
         reset: function () {
-            _player = null;
-            _isMarked = false;
-            _cellRef.attr("class", "tokenNormal");
+            mark = '';
+            cellRef.attr("class", "tokenNormal");
         }
     };
 };
