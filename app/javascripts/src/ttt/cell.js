7fe5e853e773306b7788f808f685ea338308e216
Norman Selheim <npselheim@gmail.com>
2013-02-13 08:49:57 -0500

    update comments

--- app/javascripts/src/ttt/cell.js
+++ app/javascripts/src/ttt/cell.js
@@ -1,24 +1,25 @@
 /** 
-    Creates a new Cell
-    @constructor
-    @param {Number} index the index of the cell in the grid, 0-8
-    @throws {Error} if index is out of range
-    */
+  * @namespace
+  * @param {Number} index the index of the cell in the grid, 0-8
+  * @throws {Error} if index is out of range
+  */
 MyApp.cell = function (index) {
     /** 
-        a reference to the cell in the document
-        @type {Element}
-        @private
-        */
+      * a reference to the cell in the document
+      * @type {Element}
+      * @private
+      */
     var _cellRef,
     /** 
-        the player that marked this cell
-        @type {Player}
-        @private
-        */
-        _player = null,
+      * the player that marked this cell
+      * @type {Player}
+      * @private
+      */
+        _player = '',
     /**
-        indicates whether the cell is 'taken'
+      * indicates whether the cell is 'taken'
+      * @type {Boolean}
+      * @private
       */
         _isMarked = false;
 
@@ -28,44 +29,46 @@ MyApp.cell = function (index) {
     }
     _cellRef = $('cell' + index);
 
+    /** @scope MyApp.cell */
     return {  
         /**
-            Indicates that a cell is is claimed by a player, i.e. records
-            the player's move.
-            @ param {Player} player the player that moved on this cell
+          * Indicates that a cell is is claimed by a player,
+          * i.e. records the player's move.
+          * @ param {Player} player the player that moved on this cell
           */
-        setPlayer: function (player) {
-            _player = player;
+        setPlayer: function (value) {
+            _player = value;
             _isMarked = true;
         },
-
         /**
-            Retrieves a refernce to the player who moved on the cell
-            @return {Player} the player who owns this cell
-            */
+          * Retrieves a reference to the player who marked this cell
+          * @return {Player} the player who marked this cell
+          */
         getPlayer: function() {
             return _player;
         },
-
+        /**
+          * Indicates the state of the cell, marked or not
+          * @return {Boolean} true if the cell has been marked
+          */
         isMarked: function () {
-            console.log(_player);
             return _isMarked;
         },
-
         /**
-            Show that this cell is included in the winning 3-in-a-row.
-         */
+          * Show that this cell is included in the winning 3-in-a-row.
+          * @return nothing
+          */
         showWin: function () {
             _cellRef.attr("class", "tokenWin");
         },
-
         /**
-            Reset this this cell back to the initial unused state.
-         */
+          * Reset this this cell back to the initial unused state.
+          * @return nothing
+          */
         reset: function () {
             _player = null;
             _isMarked = false;
             _cellRef.attr("class", "tokenNormal");
         }
     };
-}
+};
