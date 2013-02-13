0aae0a51641c95f78e07c179349b6082a6acac14
Norman Selheim <npselheim@gmail.com>
2013-02-13 10:31:02 -0500

    refactor MyApp.Cell
    change structure of Cell module to Module that Creates a Constructor

--- app/javascripts/src/ttt/cell.js
+++ app/javascripts/src/ttt/cell.js
@@ -3,7 +3,7 @@
   * @param {Number} index the index of the cell in the grid, 0-8
   * @throws {Error} if index is out of range
   */
-MyApp.cell = function (index) {
+MyApp.Cell = (function () {
     /** 
       * a reference to the cell in the document
       * @type {Element}
@@ -15,16 +15,20 @@ MyApp.cell = function (index) {
       * @type {Player}
       * @private
       */
-        mark = '';
+        mark = '',
 
-    // verify that index is in range 0-8
-    if ( index < 0 || index > 8 ) {
-        throw new Error( "index must be in range 0-8" );
-    }
-    cellRef = $('cell' + index);
+        Constr;
 
-    /** @scope MyApp.cell */
-    return {  
+    Constr = function (index) {
+        // verify that index is in range 0-8
+        if ( index < 0 || index > 8 ) {
+            throw new Error( "index must be in range 0-8" );
+        }
+        this.cellRef = $('cell' + index);
+    };
+
+    Constr.prototype = {
+        constructor: MyApp.Cell,
         /**
           * Indicates that a cell is is claimed by a player,
           * i.e. records the player's move.
@@ -34,36 +38,39 @@ MyApp.cell = function (index) {
             if ( value !== 'X' && value !== 'O' ) {
                 throw new Error( "mark must be 'X' or 'O'" );
             };
-            mark = value;
+            this.mark = value;
         },
         /**
           * Retrieves a reference to the player who marked this cell
           * @return {Player} the player who marked this cell
           */
         getMark: function() {
-            return mark;
+            return this.mark;
         },
         /**
           * Indicates the state of the cell, marked or not
           * @return {Boolean} true if the cell has been marked
           */
         isMarked: function () {
-            return mark ? true : false;
+            return this.mark ? true : false;
         },
         /**
           * Show that this cell is included in the winning 3-in-a-row.
           * @return nothing
           */
         showWin: function () {
-            cellRef.attr("class", "tokenWin");
+            this.cellRef.attr("class", "tokenWin");
         },
         /**
           * Reset this this cell back to the initial unused state.
           * @return nothing
           */
         reset: function () {
-            mark = '';
-            cellRef.attr("class", "tokenNormal");
+            this.mark = '';
+            this.cellRef.attr("class", "tokenNormal");
         }
     };
-};
+
+    return Constr;
+    
+}());
